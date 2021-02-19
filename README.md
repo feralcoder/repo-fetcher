cbd4a7cfed5e14ef774512c34a15bf2c0eaf4d2b
feralcoder <feralcoder@gmail.com>
2021-02-19 23:24:07 +0000

    Tools to set up, maintain, and use local package repos.

---
+++ README.md
@@ -0,0 +1,2 @@
+With a little help from:
+https://computingforgeeks.com/how-to-create-centos-8-local-repository-mirrors-with-rsync-nginx/
---
+++ set_client_repos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+THIS_SOURCE="$(dirname ${BASH_SOURCE[0]})"
+ABS_PATH=$( readlink -f $THIS_SOURCE )
+echo Running scripts from: $THIS_SOURCE
+echo Apsolute path is: $ABS_PATH
+echo
+
+
+if [[ $USER != root ]]; then
+  echo "Must be root to run this!"
+  exit
+fi
+
+REPOIP=192.168.127.220
+
+cd /etc/yum.repos.d/
+mkdir old-repos
+mv *.repo old-repos
+mv old-repos/CentOS-Linux-Sources.repo .
+
+cat $ABS_PATH/feralcoder.repo | sed "s|<<REPOIP>>|$REPOIP|g" > ./feralcoder.repo
+
+yum clean all
+yum makecache
+yum repolist
+yum -y update
+yum install yum-utils
+yum-config-manager --enable PowerTools
+#yum-config-manager --enable centosplus
